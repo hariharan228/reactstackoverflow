3c69b52328f02b405c779c8b2541f9818cbb402d
Hariharan E <e.hari.haran.98@gmail.com>
2021-01-03 18:55:52 +0530

    Routes created

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(db,{
+            useCreateIndex:true,
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useFindAndModify:false
+        
+        });
+        console.log('Mongodb connected');
+    }
+    catch(err){
+        console.error(err.message);
+        process.exit(1);
+    }
+}
+
+
+
+module.exports = connectDB;
